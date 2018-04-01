a11da6ef51eeea6860f88504b235d935d348347a
siddsach <ssachdeva@uchicago.edu>
2018-04-01 15:22:04 -0400

    finished and passed tests for adasoft module

--- allennlp/modules/adasoft.py
+++ allennlp/modules/adasoft.py
@@ -1,9 +1,7 @@
-from typing import List
+from typing import List, Dict
 import torch
-from overrides import overrides
 
-from allennlp.common import Params
-from allennlp.nn.util import zeros_like
+#from allennlp.common import Params
 
 class AdaptiveSoftmax(torch.nn.Module):
     """
@@ -12,6 +10,7 @@ class AdaptiveSoftmax(torch.nn.Module):
     on GPUs. It can speed up softmax computation over large
     vocabularies 2-10x. For more detail, read the paper
     at https://arxiv.org/pdf/1609.04309.pdf
+            # assumes indices start from 0 with decreasing frequency
     Parameters
     ----------
     input_size : ``int``, required
@@ -20,26 +19,26 @@ class AdaptiveSoftmax(torch.nn.Module):
         Number of tokens in each class
     """
 
-    def __init__(self, input_size: int, class_cutoff_sizes: List[int]) -> None:
+    def __init__(self, input_size: int, vocab_size: int, class_cutoff_sizes: List[int], tail_projection_ratio: int = 4) -> None:
         super(AdaptiveSoftmax, self).__init__()
 
         self.input_size = input_size
-        self.class_cutoff_sizes = class_cutoff_sizes
-        self.output_size = class_cutoff_sizes[0] + len(class_cutoff_sizes) - 1
+        self.class_cutoff_sizes = class_cutoff_sizes + [vocab_size]
+        self.output_size = class_cutoff_sizes[0] + len(self.class_cutoff_sizes) - 1
 
         self.head = torch.nn.Linear(input_size, self.output_size)
         self.tail = torch.nn.ModuleList()
 
-        for tail_class_i in range(len(class_cutoff_sizes) - 1):
+        for tail_class_i in range(len(self.class_cutoff_sizes)-1):
             capacity_reduction_layer = torch.nn.Sequential(
-                torch.nn.Linear(input_size, input_size // 4 ** tail_class_i, False),
-                torch.nn.Linear(input_size // 4 ** tail_class_i, class_cutoff_sizes[tail_class_i + 1] - class_cutoff_sizes[tail_class_i], False)
+                torch.nn.Linear(input_size, input_size // tail_projection_ratio ** tail_class_i, False),
+                torch.nn.Linear(input_size // tail_projection_ratio ** tail_class_i, self.class_cutoff_sizes[tail_class_i + 1] - self.class_cutoff_sizes[tail_class_i], False)
             )
 
             self.tail.append(capacity_reduction_layer)
 
         self.logsoftmax = torch.nn.LogSoftmax()
-        self.criterion = torch.nn.CrossEntropyLoss(size_average=False)
+        self.criterion = torch.nn.CrossEntropyLoss(reduce = False, size_average=False)
 
 
     def reset(self):
@@ -49,18 +48,25 @@ class AdaptiveSoftmax(torch.nn.Module):
             torch.nn.init.xavier_normal(tail[0].weight)
             torch.nn.init.xavier_normal(tail[1].weight)
 
-    def set_target(self, target):
+    def set_target(self, target: torch.LongTensor) -> None:
         '''
+        input_shape: (batch_size, 1)
+
         function to determine which clusters in the tree have words that are actually
-        in the target. to prevent unnecessarily computing softmax over classes without
-        the right answer
+        in the target ouput. This allows us to save computation in training by only
+        computing loss for the clusters that contribute some loss
         '''
         self.target_indices = len(self.tail) * [None]
 
         for tail_class_i in range(len(self.tail)):
-            mask = target.ge(self.class_cutoff_sizes[tail_class_i]).mul(target.lt(self.class_cutoff_sizes[tail_class_i + 1]))
+            # find elements with indices in this class
+            # (batch_size, 1)
+            mask = target.data.ge(self.class_cutoff_sizes[tail_class_i]) \
+                         .mul(target.data.lt(self.class_cutoff_sizes[tail_class_i + 1]))
 
+            #Check if this batch has targets in tail_class_i
             if mask.sum() > 0:
+                #(mask.float().nonzero().sum(), 1)
                 self.target_indices[tail_class_i] = (torch.autograd.Variable(mask.float().nonzero().squeeze(1)))
 
 
@@ -70,23 +76,42 @@ class AdaptiveSoftmax(torch.nn.Module):
         else:
             return self.log_prob(input)
 
-    def adasoft(self, input):
+    def adasoft(self, input:torch.Tensor) -> List[torch.Tensor]:
+        '''
+        input_shape: (batch_size, hidden_size)
+        output_shape: [(batc
+        [(batch_size, class_cutoff_sizes[0]), (batch_size, class_cutoff_sizes[1]-class_cutoff_sizes[0]), ..., (batch_size, vocab_size- class_cutoff_sizes[-1])]
+
+        Computes projection from hidden state to class scores, where
+        the class scores are sorted in a list of classes based on
+        word indices (assumed to be sorted by indices) and
+        '''
+
         #Compute scores for words in head cluster and all the other clusters
         output = len(self.class_cutoff_sizes)* [None]
         output[0] = self.head(input)
 
         for tail_class_i in range(len(self.target_indices)):
             if self.target_indices[tail_class_i] is not None:
+
                 # If any words in tail cluster i+1 are in the target than
                 # consider those
-                output[tail_class_i] = (self.tail[tail_class_i](input.index_select(0, self.target_indices[tail_class_i])))
+                # (num_rows_with_targets_in_class_i, hidden_size)
+                rows_with_targets_in_class_i= input.index_select(0, self.target_indices[tail_class_i])
+
+                #Only compute projection on examples with targets in this class
+                # (num_rows_with_targets_in_class_i, num_vocab_in_class_i)
+                output[tail_class_i+1] = (self.tail[tail_class_i](rows_with_targets_in_class_i))
 
         return output
 
-    def log_prob(self, input):
+    def log_prob(self, input: torch.Tensor) -> torch.Tensor:
         '''
-        convert the hierarchical probabalities to flat probabilities to run on
-        non-training data
+        input_shape: (batch_size, hidden_size)
+        output_shape: (batch_size, vocab_size)
+
+        convert the adasoft hierarchical probabalities to flat probabilities to run
+        on non-training data
         '''
 
         head_probs = self.head(input)
@@ -98,86 +123,92 @@ class AdaptiveSoftmax(torch.nn.Module):
 
         lsm_head = self.logsoftmax(head_probs)
 
-        #narrow prob to only the number of elements in head = (#vocab in head + #tail clusters)
-        prob.narrow(1, 0, self.output_size).add_(lsm_head.narrow(1, 0, self.output_size).data)
+        #add the prababilities in the head
+        prob.narrow(1, 0, self.output_size).add_(lsm_head.data)
 
         for tail_class_i in range(len(self.tail)):
-            #get the starting index of class i
-            pos = self.class_cutoff_sizes[tail_class_i]
-            i_size = self.class_cutoff_sizes[tail_class_i + 1] - pos
+            # get the starting and size of class i
+            i_start = self.class_cutoff_sizes[tail_class_i]
+            i_size = self.class_cutoff_sizes[tail_class_i + 1] - i_start
 
-            #get probability of tail class i
+            # get log probability of tail class i
             buffer = lsm_head.narrow(1, self.class_cutoff_sizes[0] + tail_class_i, 1)
 
-            #get the word probabalities within class i
+            # get the word log-probabalities within class i
             lsm_tail = self.logsoftmax(self.tail[tail_class_i](input))
 
-            # here it's calculating probabilities for the tail classes by adding
-            # the within class probabilities to the whole class probability
-            # ???????????????????????
-            # adds probabilities because its the log!
+            # adds probabilities because its the log
             buffer = buffer.expand(batch_size, i_size)
-            prob.narrow(1, pos, i_size).copy_(buffer.data).add_(lsm_tail.data)
+            prob.narrow(1, i_start, i_size).copy_(buffer.data).add_(lsm_tail.data)
 
         return prob
 
-    def remap_target(self, target: torch.Tensor) -> List[torch.Tensor]:
+    def _remap_target(self, target: torch.Tensor) -> List[Dict[str, torch.Tensor]]:
         '''
-        input shape: (batch * max_len, 1)
-        output shape: List[(batch * max_len, 1)]
+        input shape: (batch_size, 1) NOT A VARIABLE
+        output shape: List[(batch_size, 1)]
+
+        Converts target tensor to list of tensors for computing loss after adasoft
+        classwise
         '''
 
-        # (batch * max_len, 1)
-        new_target = [target.clone()]
+        new_target = len(self.class_cutoff_sizes) * [None]
+        new_target[0] = {"targets": target.clone(), "mask":torch.ones_like(target)}
 
         for tail_class_i in range(len(self.class_cutoff_sizes) - 1):
 
             # get all the indices for elements in the target in class i
             # (batch * max_len, 1)
-            # assumes indices start from 0 with decreasing frequency
             mask = target.ge(self.class_cutoff_sizes[tail_class_i]) \
                          .mul(target.lt(self.class_cutoff_sizes[tail_class_i + 1]))
 
-            #set those indices in the remapped target to the class index in the head
-            new_target[0][mask] = self.class_cutoff_sizes[0] + tail_class_i
-
             if mask.sum() > 0:
+                new_target[0]["targets"][mask] = self.class_cutoff_sizes[0] + tail_class_i
+
+                # set those indices in the remapped target to the class index in the head
 
                 # if there are words in class i in the target,
-                # make the ith element of target a list of the target indices
+                # make the ith element of target a 1-d tensor of the target indices
                 # in class i, except with the indices starting from zero
-                new_target.append(target[mask].add(-self.class_cutoff_sizes[tail_class_i]))
-
-            else:
-                new_target.append(None)
+                # (num_rows_with_targets_in_class_i)
+                targets_in_class_i = target[mask].add(-self.class_cutoff_sizes[tail_class_i])
+                new_target[tail_class_i+1] = {"targets": targets_in_class_i, "mask":mask}
 
         return new_target
 
-    def compute_loss(self, log_probs: List[torch.Tensor],
+    def compute_loss(self, class_scores: List[torch.Tensor],
                            target: List[torch.Tensor]) -> torch.Tensor:
         '''
-        log_probs: [(batch_size * seq_len, num_classes), (batch_size * seq_len, num_classes)]
-        target: [(batch_size * seq_len, 1), (batch_size * seq_len, 1)...]
+        log_probs: [(batch_size, num_classes), (batch_size, num_classes)]
+        target: [(batch_size, 1), (batch_size, 1)...]
         '''
 
-        # [(batch_size * seq_len, 1), (batch_size * seq_len, 1)...]
-        target = self.remap_target(target.data)
+        # (batch_size, 1)
+        negative_log_likelihood = torch.autograd.Variable(torch.zeros(target.size(0)))
 
-        # (batch_size * seq_len, 1)
-        negative_log_likelihood = zeros_like(target[0])
+        # [(batch_size, 1), (batch_size, 1)...]
+        target = self._remap_target(target.data)
 
         # We compute loss classwise, to save compute on classes for which
         # we don't have any target indices in so we don't have to needlessly consider
         # them
-        for tail_class_i in range(len(input)):
+        for tail_class_i in range(len(class_scores)):
             # make sure this class actually has a word in the target sequence before considering
-            if log_probs[tail_class_i] is not None:
+            if class_scores[tail_class_i] is not None:
+                target_indices = target[tail_class_i]["targets"]
+                target_mask = target[tail_class_i]["mask"]
+
                 #making sure the target indices are actually in this class
-                assert(target[tail_class_i].min() >= 0 and target[tail_class_i].max() <= input[tail_class_i].size(1))
+                upper = class_scores[tail_class_i].size(1)
+                assert target_indices.min() >= 0 and target_indices.max() <= upper, \
+                        "Target indices out of range!" + str(target_indices)
+
+                target_var = torch.autograd.Variable(target_indices)
 
                 # adding the loss for class i to total loss
                 # adds probabilities because its the log!
-                # (batch_size * seq_len, 1)
-                negative_log_likelihood += -torch.gather(input=log_probs[tail_class_i], dim=1, index=target[tail_class_i])
+                # (batch_size)
+                this_loss = self.criterion(class_scores[tail_class_i], target_var)
+                negative_log_likelihood[target_mask] += this_loss
 
         return negative_log_likelihood
--- tests/modules/adasoft_test.py
+++ tests/modules/adasoft_test.py
@@ -23,28 +23,94 @@ class TestAdaptiveSoftmax(AllenNlpTestCase):
 
         zipf_probs = 1/torch.Tensor(range(1, self.vocab_size+1))
         zipf_distr = Categorical(probs = zipf_probs)
-        self.inp = torch.zeros(self.batch_size)
+        self.inp = Variable(torch.randn(self.batch_size, self.hidden_size))
+        self.target = Variable(torch.LongTensor(self.batch_size))
+        self.target[:] = 0
         for i in range(self.batch_size):
-            self.inp = zipf_distr.sample()
+            self.target[i] = zipf_distr.sample()[0]
+
 
     def test_set_target(self):
         self.adasoft.set_target(self.target)
 
-        for c in self.class_cutoff_sizes:
+        for i, c in enumerate(self.class_cutoff_sizes):
             #get indices in class c
-            indices_in_this_class = (self.target > self.class_cutoff_sizes[c]) * (self.target < self.class_cutoff_sizes[c+1])
+            indices_in_this_class = (self.target > c) * (self.target < self.adasoft.class_cutoff_sizes[i+1])
 
             #Check that we're considering the right classes
-            assert bool(self.adasoft.target_indices[c]) == bool(indices_in_this_class.sum())
-            if self.adasoft.target_indices[c]:
-                assert self.adasoft.target_indices[c].data == indices_in_this_class.float().nonzero().squeeze(1)
+            assert (self.adasoft.target_indices[i] is None) == (indices_in_this_class.data.sum() == 0)
+            try:
+                if self.adasoft.target_indices[i]:
+                    assert (self.adasoft.target_indices[i].data == indices_in_this_class.float().nonzero().squeeze(1)).data[0]
+            except:
+                ValueError("Got " + str(self.adasoft.target_indices[i]) + "instead of:" + str(indices_in_this_class))
 
-    #def test_adasoft_projection(self):
+
+    def test_adasoft_projection(self):
+        self.adasoft.set_target(self.target)
+
+        output = self.adasoft.adasoft(self.inp)
+
+        assert output[0].shape == (self.batch_size, self.class_cutoff_sizes[0] + len(self.class_cutoff_sizes)), output[0].shape
+
+        for i, tail_class_i in enumerate(output[1:]):
+            if self.adasoft.target_indices[i] is None:
+                assert tail_class_i is None, "Expected {} got {}".format(None, tail_class_i)
+            else:
+                try:
+                    expected_shape = (self.adasoft.target_indices[i].size(0), self.adasoft.class_cutoff_sizes[i+1] - self.adasoft.class_cutoff_sizes[i])
 
 
+                    assert tail_class_i.shape == expected_shape
+                except:
+                    ValueError('Expected {} Got {}'.format(expected_shape, tail_class_i.shape))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_logprob(self):
+        probs = self.adasoft.log_prob(self.inp)
+        assert probs.shape == (self.batch_size, self.vocab_size)
+        sum_to_one = probs.exp().sum(dim=1)
+        assert_almost_equal((sum_to_one-torch.ones(self.batch_size)).sum(), 0)
+
+    def test_remap_target(self):
+        targ = self.target.data
+        remapped_target = self.adasoft._remap_target(targ)
+
+        remapped_target_indices = [c["targets"] if c else None for c in remapped_target]
+
+        assert remapped_target_indices[0].shape == (self.batch_size,)
+
+        for i, c in enumerate(self.class_cutoff_sizes):
+            #get indices in class c
+            indices_in_this_class = (targ > c) * (targ < self.adasoft.class_cutoff_sizes[i+1])
+
+            #Check that we're considering the right classes
+            assert (remapped_target_indices[i+1] is None) == (indices_in_this_class.sum() == 0)
+            if remapped_target_indices[i+1] is not None:
+                expected_indices_in_this_class = remapped_target_indices[i+1] + c
+                assert (expected_indices_in_this_class == self.target[indices_in_this_class].data).sum() == len(expected_indices_in_this_class)
+
+    def test_compute_loss(self):
+        criterion = torch.nn.CrossEntropyLoss()
+
+        self.adasoft.set_target(self.target)
+        log_probs = self.adasoft.adasoft(self.inp)
+        loss = self.adasoft.compute_loss(log_probs, self.target)
+        remapped_target = self.adasoft._remap_target(self.target.data)
+
+        assert loss.shape == (self.batch_size,), loss.shape
+
+        expected_loss = 0
+
+        for i in range(len(log_probs)):
+            if log_probs[i] is not None:
+                new_target = torch.autograd.Variable(remapped_target[i]["targets"])
+                expected_loss += criterion(log_probs[i], new_target)
+
+        try:
+            assert_almost_equal(loss.sum(), expected_loss)
+        except:
+            ValueError("Expected {} Got {}".format(expected_loss, loss.sum()))
+
 
 
 
